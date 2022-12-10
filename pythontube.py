c8a900716c70bbcdf7efac5953d34bf4feb193c5
BigSmoke010 <behirimohamedcherif0@gmail.com>
2022-12-10 01:06:34 +0100

    formatted files using yapf

--- pythontube.py
+++ pythontube.py
@@ -7,18 +7,30 @@ root = Tk()
 option = IntVar()
 option.set(0)
 vidtru = 0
+
+
 def changevarz():
     global vidtru
     vidtru = 0
+
+
 def changevary():
     global vidtru
     vidtru = 1
 
-vdio_format = Radiobutton(root, variable=option, value=0, text='mp4', command=changevarz)
+
+vdio_format = Radiobutton(root,
+                          variable=option,
+                          value=0,
+                          text='mp4',
+                          command=changevarz)
 vdio_format.grid(row=0, column=0)
 
-
-plylist_format = Radiobutton(root, variable=option, value=1, text='mp3', command=changevary)
+plylist_format = Radiobutton(root,
+                             variable=option,
+                             value=1,
+                             text='mp3',
+                             command=changevary)
 plylist_format.grid(row=0, column=2)
 
 lbl = Label(root, text='Please input the Video link')
@@ -31,6 +43,7 @@ root.rowconfigure(0, minsize=50)
 
 root.columnconfigure(2, minsize=100)
 root.rowconfigure(3, minsize=50)
+
 
 def submitvideo():
     vd = YouTube(inpt.get())
@@ -46,6 +59,7 @@ def submitvideo():
         os.rename(outvid, new_file)
         print('succesfully downloaded')
 
+
 sbmit = Button(root, text='Submit', command=submitvideo)
 sbmit.grid(row=2, column=1)
 
@@ -54,12 +68,14 @@ lbl.grid(row=3, column=1)
 inputt = Entry(root, width=50)
 inputt.grid(row=4, column=1)
 
+
 def submitplaylist():
     vd = Playlist(inputt.get())
     for i in vd.videos:
         if vidtru == 0:
             print('Downloading : ' + i.title)
-            i.streams.get_highest_resolution().first().download(output_path='downloads/')
+            i.streams.get_highest_resolution().first().download(
+                output_path='downloads/')
             print('succesfully downloaded')
         else:
             print('Downloading : ' + i.title)
@@ -69,6 +85,7 @@ def submitplaylist():
             new_file = base + '.mp3'
             os.rename(outvid, new_file)
             print('succesfully downloaded')
+
 
 sbmit = Button(root, text='Submit', command=submitplaylist)
 sbmit.grid(row=5, column=1)
